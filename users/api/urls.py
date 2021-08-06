c3ac3e7a2f1b6fd61d8e1731f623c16bf8583c99
Anton Minski <anton_minski5@ukr.net>
2021-08-06 18:51:18 +0300

    04 viewsets, routers

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,6 +1,27 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from . import views
 
+
+router = DefaultRouter()
+router.register(r'profiles', views.UserProfileViewSet)
+router.register(r'messages', views.MessageViewSet)
+
+profile_list = views.UserProfileViewSet.as_view({'get': 'list'})
+profile_detail = views.UserProfileViewSet.as_view({'get': 'retrieve'})
+
+
+
+
+
 urlpatterns = [
-    path('profiles/', views.UserProfileList.as_view(), name='user-profile-list')
+    # path('profiles/', views.UserProfileList.as_view(), name='user-profile-list')
+
+    # path('profiles/', profile_list, name='user-profile-list'),
+    # path('profiles/<int:pk>/', profile_detail, name='user-profile-detail'),
+
+    path("", include(router.urls)),
+    path('avatar/', views.AvatarUpdateView.as_view(), name='avatar-update'),
+
 ]
--- users/api/views.py
+++ users/api/views.py
@@ -1,16 +1,46 @@
 from rest_framework import generics, mixins, viewsets
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import mixins, viewsets
 from rest_framework.viewsets import ModelViewSet
 
 # from users.api.permissions import IsOwnerOrReadOnly, IsOwnProfileOrReadOnly
-from users.api.serializers import UserProfileSerializer, \
+from .serializers import UserProfileSerializer, \
     UserProfileAvatarSerializer, MessageSerializer
+from .permissions import IsOwnPofileOrRead_only, IsOwnerOrReadOnly
 
 from users.models import UserProfile, Message
 
-
+'''
 class UserProfileList(generics.ListAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     permission_classes = [IsAuthenticated]
+'''
+
+# same on viewsets:
+class UserProfileViewSet(mixins.UpdateModelMixin, mixins.ListModelMixin,
+                         mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated, IsOwnPofileOrRead_only]
+
+
+class MessageViewSet(ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        sender = self.request.user.userprofile
+        serializer.save(sender=sender)
+
+
+class AvatarUpdateView(generics.UpdateAPIView):
+    # queryset = Message.objects.all()
+    serializer_class = UserProfileAvatarSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        profile_object = self.request.user.userprofile
+        return profile_object
